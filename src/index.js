3e7dd5c7eccd33e1e02da6543031fc4e2f3e39ea
Jaaki <jaakidup@gmail.com>
2019-05-09 21:19:01 +0200

    Updated query

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import "./styles.css";
 let bodybuilder = require("bodybuilder");
 let build = bodybuilder()
   .query("match", "name", "Tessa")
-  .query("gte", "age", 18)
-  .query("lte", "age", 38)
-  .query("range", "price", {
-    gte: 10,
-    lte: 320
+  // .query("gte", "age", 18)
+  // .query("lte", "age", 38)
+  .query("range", "age", {
+    gte: 18,
+    lte: 38
   })
   .build();
 
